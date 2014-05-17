8e65c3ba4ffdddae1a876eab78b2f992e2220a40
mkulesz <michal.kulesz@gmail.com>
2014-05-17 18:21:04 +0200

    Some wierd stuff :)
    Added window

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-
-public class Main {
-
+import javax.swing.JFrame;
+public class Main extends JFrame {
+	public static void main (String args[]){
+		Main gui = new Main();
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setSize(300, 300);
+		gui.setVisible(true);
+		gui.setTitle("Gui");
+	}
 }
