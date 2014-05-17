fc3a62785e22bda041e2d65ff2f99e9acedcfe31
Necromos <masternecromos@gmail.com>
2014-05-17 18:16:30 +0200

    remake weka files

---
+++ src/ClassifierWrapper.java
@@ -0,0 +1,14 @@
+import weka.classifiers.evaluation.*;
+import weka.classifiers.trees.j48.*;
+import weka.core.*;
+
+public class ClassifierWrapper {
+	
+	ClassifierWrapper (){
+		
+	}
+	
+	void analyzeData(){
+		
+	}
+}
