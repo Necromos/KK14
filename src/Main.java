612979000e314dd5791fc8207a09a910452cfd3f
mkulesz <michal.kulesz@gmail.com>
2014-05-17 19:21:33 +0200

    Some Layout

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 public class Main extends JFrame {
+	private JLabel Label;
+	private JButton button;
+	
+	public Main()	{
+		setLayout(new FlowLayout());
+		
+		Label = new JLabel("Hi!");
+		add(Label);
+		setLayout(new GridLayout(6, 4));
+		add(new Checkbox("one", null, true));
+		add(new Checkbox("two"));
+		add(new Checkbox("three"));
+		add(new Checkbox("four"));
+		
+		button = new JButton("Check!");
+		add(button);
+	}
+	
 	public static void main (String args[]){
 		Main gui = new Main();
 		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
