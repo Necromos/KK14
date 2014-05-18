ab7198e9e07117e0bca625d23902f93b08b7c035
Necromos <masternecromos@gmail.com>
2014-05-18 09:24:22 +0200

    changed v of weka and added boilerplate

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import javax.swing.JFrame;
 public class Main extends JFrame {
+
 	public static void main (String args[]){
 		Main gui = new Main();
 		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
