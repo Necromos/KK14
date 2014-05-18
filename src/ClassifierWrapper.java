2e16001a1481fab24dd35d6cf79e1f241ddab623
Necromos <masternecromos@gmail.com>
2014-05-18 10:10:43 +0200

    working wrapper code =D

--- src/ClassifierWrapper.java
+++ src/ClassifierWrapper.java
@@ -64,13 +64,15 @@ public class ClassifierWrapper {
 		
 		Instances data = new Instances(datafile);
 		data.setClassIndex(data.numAttributes() - 1);
- 
+		BufferedReader testFile = readDataFile("D:/breast-cancer-test.arff");
+		Instances test = new Instances(testFile);
+		test.setClassIndex(test.numAttributes() - 1);
 		// Do 10-split cross validation
-		Instances[][] split = crossValidationSplit(data, 10);
+//		Instances[][] split = crossValidationSplit(data, 10);
  
 		// Separate split into training and testing arrays
-		Instances[] trainingSplits = split[0];
-		Instances[] testingSplits = split[1];
+//		Instances[] trainingSplits = split[0];
+//		Instances[] testingSplits = split[1];
  
 		// Use a set of classifiers
 		Classifier[] models = { 
@@ -87,14 +89,14 @@ public class ClassifierWrapper {
 			FastVector predictions = new FastVector();
  
 			// For each training-testing split pair, train and test the classifier
-			for (int i = 0; i < trainingSplits.length; i++) {
-				Evaluation validation = classify(models[j], trainingSplits[i], testingSplits[i]);
- 
+			//for (int i = 0; i < trainingSplits.length; i++) {
+//				Evaluation validation = classify(models[j], trainingSplits[i], testingSplits[i]);
+				Evaluation validation = classify(models[j], data, test);
 				predictions.appendElements(validation.predictions());
  
 				// Uncomment to see the summary for each training-testing pair.
 				//System.out.println(models[j].toString());
-			}
+			//}
  
 			// Calculate overall accuracy of current classifier on all splits
 			double accuracy = calculateAccuracy(predictions);
